a7a9d1250dd7d50b69e14f8295e43905cca82b6f
Naveen Chandar J <naverdocker@gmail.com>
2025-10-31 01:19:20 +0530

    updating highlights

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# nvim-config
+## nvim-config
 
-## Included Configurations
+### Included Configurations
 
 - This repository contains config for primarily Neovim
 - however also contain config for vim to try to maintain equivalent version.
--- lua/highlights.lua
+++ lua/highlights.lua
@@ -6,6 +6,13 @@ function M.default_apply()
     vim.api.nvim_set_hl(0, "MsgArea", { fg = "#c0c0c0" })
     vim.api.nvim_set_hl(0, "StatusLine", { fg = "#c0c0c0", bg = "#1e1e1e", bold = true })
     vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#707070", bg = "#1e1e1e" })
+    vim.cmd [[
+      hi Normal guibg=none ctermbg=none
+      hi LineNr guibg=none ctermbg=none
+      hi SignColumn guibg=none ctermbg=none
+      hi EndOfBuffer guibg=none ctermbg=none
+      hi WinSeparator guibg=none ctermbg=none
+    ]]
 end
 
 return M
