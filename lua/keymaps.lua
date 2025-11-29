f9a03afd2c21bd0d4f1d2f2b82ce0d33b956c567
Naveen Chandar J <naverdocker@gmail.com>
2025-11-29 23:08:47 +0530

    enable: mapping to send-keys current line as comment to another tmux pane and send-keys visual selection as command to another tmux pane

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,7 +8,7 @@ vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
 --setting leader key
 vim.g.mapleader = ','
 
-vim.keymap.set('n', '<leader>r', ':source ~/.config/nvim/init.lua<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>t', ':source ~/.config/nvim/init.lua<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>wa', ':wa<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>wqa', ':wqa<CR>', { noremap = true, silent = true })
@@ -43,36 +43,9 @@ vim.keymap.set('i', '<leader>{', '{}<Left>', { noremap = true, silent = true })
 vim.keymap.set('i', '<leader>[', '[]<Left>', { noremap = true, silent = true })
 vim.keymap.set('i', '<leader><CR>', '<CR><up><esc>A', { noremap = true, silent = true })
 
--- tmux: save current file and executing current file in tmux bottom pane
-vim.keymap.set('n', '<leader>ww', function()
-    vim.cmd('write')
-    local file = vim.fn.expand('%:p')
-    local name = vim.fn.expand('%:t')
-    vim.fn.system('tmux send-keys -t bottom "' .. file .. '" Enter')
-    print("▶ Sent to tmux bottom: " .. name)
-end, { noremap = true, silent = true })
-
--- tmux: clear bottom tmux pane
-vim.keymap.set('n', '<leader>wc', function()
-    vim.fn.system('tmux send-keys -t bottom "clear" Enter')
-    print("🧹 Cleared tmux bottom pane")
-end, { noremap = true, silent = true })
-
--- tmux: save current file and executing current file in tmux right pane
-vim.keymap.set('n', '<leader>wr', function()
-    vim.cmd('write')
-    local file = vim.fn.expand('%:p')
-    local name = vim.fn.expand('%:t')
-    vim.fn.system('tmux send-keys -t right "' .. file .. '" Enter')
-    print("▶ Sent to tmux right: " .. name)
-end, { noremap = true, silent = true })
-
--- tmux: clear right tmux pane
-vim.keymap.set('n', '<leader>rc', function()
-    vim.fn.system('tmux send-keys -t right "clear" Enter')
-    print("🧹 Cleared tmux right pane")
-end, { noremap = true, silent = true })
-
+-------------------------
+-- TOGGLE LINE COMMENT --
+-------------------------
 -- toggle comment for current line (# based)
 vim.keymap.set('n', '<leader>3', function()
     local lnum = vim.fn.line('.')
@@ -86,3 +59,81 @@ vim.keymap.set('n', '<leader>3', function()
 
     vim.fn.setline(lnum, line)
 end, { silent = true, desc = 'Toggle comment (normal)' })
+
+-------------------
+-- TMUX REPL - 1 --
+-------------------
+-- BOTTOM PANE: save current file and executing current file in tmux BOTTOM pane
+vim.keymap.set('n', '<leader>ww', function()
+    vim.cmd('write')
+    local file = vim.fn.expand('%:p')
+    local name = vim.fn.expand('%:t')
+    vim.fn.system('tmux send-keys -t bottom "' .. file .. '" Enter')
+    print("▶ Sent to tmux bottom: " .. name)
+end, { noremap = true, silent = true })
+
+-- BOTTOM PANE: clear bottom tmux pane
+vim.keymap.set('n', '<leader>wc', function()
+    vim.fn.system('tmux send-keys -t bottom "clear" Enter')
+    print("🧹 Cleared tmux bottom pane")
+end, { noremap = true, silent = true })
+
+-- RIGHT PANE: save current file and executing current file in tmux right pane
+vim.keymap.set('n', '<leader>wr', function()
+    vim.cmd('write')
+    local file = vim.fn.expand('%:p')
+    local name = vim.fn.expand('%:t')
+    vim.fn.system('tmux send-keys -t right "' .. file .. '" Enter')
+    print("▶ Sent to tmux right: " .. name)
+end, { noremap = true, silent = true })
+
+-- RIGHT PANE: clear right tmux pane
+vim.keymap.set('n', '<leader>rc', function()
+    vim.fn.system('tmux send-keys -t right "clear" Enter')
+    print("🧹 Cleared tmux right pane")
+end, { noremap = true, silent = true })
+
+-------------------
+-- TMUX REPL - 2 --
+-------------------
+-- BOTTOM PANE: send highlighted selection as command to bottom tmux pane
+vim.keymap.set("v", "<leader>v", function()
+  -- 1. Yank visual selection to register 'v'
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg("v")
+
+  -- 2. Send keys to the pane directly below ('-t {down-of}')
+  -- Change '{down-of}' to '.+1' if you simply want the next pane index
+  vim.fn.system({"tmux", "send-keys", "-t", "{down-of}", text, "Enter"})
+end, { desc = "Send highlighted text to bottom tmux pane" })
+
+-- BOTTOM PANE: send current line as command to bottom tmux pane
+vim.keymap.set("n", "<leader>vv", function()
+  local text = vim.api.nvim_get_current_line()
+
+  -- Send text + Enter to the pane below
+  vim.fn.system({"tmux", "send-keys", "-t", "{down-of}", text, "Enter"})
+
+  -- Optional: Print confirmation message
+  print("Sent to tmux: " .. text)
+end, { desc = "Send current line to bottom tmux pane" })
+
+-- RIGHT PANE: send highlighted selection as command to bottom tmux pane
+vim.keymap.set("v", "<leader>r", function()
+  -- Yank visual selection to register 'v'
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg("v")
+
+  -- Send to pane right of current
+  vim.fn.system({"tmux", "send-keys", "-t", "{right-of}", text, "Enter"})
+end, { desc = "Send visual selection to right tmux pane" })
+
+
+-- RIGHT PANE: send current line as command to bottom tmux pane
+vim.keymap.set("n", "<leader>rr", function()
+  local text = vim.api.nvim_get_current_line()
+
+  -- Send to pane right of current
+  vim.fn.system({"tmux", "send-keys", "-t", "{right-of}", text, "Enter"})
+end, { desc = "Send current line to right tmux pane" })
+
